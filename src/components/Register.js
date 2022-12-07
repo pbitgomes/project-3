41adebbdeaebf31b8d5a4cce2a8294a072faec44
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-12-07 10:48:50 -0300

    cloudinary

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import api from "../api/api.js";
 
 function Register() {
   const navigate = useNavigate();
+  const [img, setImg] = useState("");
   const [form, setForm] = useState({
     name: "",
     email: "",
@@ -15,13 +17,38 @@ function Register() {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleImage = (e) => {
+    setImg(e.target.files[0]);
+  };
+
+  // faz a requisição d imagem (/upload-image) e retorna o path (caminho da imagem)
+  const handleUpload = async () => {
+    try {
+      const uploadData = new FormData();
+      uploadData.append("picture", img);
+
+      // subindo a imagem para o cloudinary
+      const response = await api.post("/uload-image", uploadData);
+
+      // retorna o caminho (path) da imagem dentro do cloudinary
+      return response.data.url;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // criar a requisição para enviar este novo usuário ao BD
-    // requisição do método POST
-
-    navigate("/login");
+    try {
+      const imgURL = await handleUpload()
+      // criar a requisição para enviar este novo usuário ao BD
+      // requisição do método POST
+      await api.post("user/register", {...form, profileImg: imgURL})
+  
+      navigate("/login");
+    } catch (error){
+      console.log(error)
+    }
   };
 
   return (
@@ -47,6 +74,11 @@ function Register() {
             value={form.email}
             onChange={handleChange}
           />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Imagem de perfil</Form.Label>
+          <Form.Control type="file" onChange={handleImage} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
